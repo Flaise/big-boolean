a7c941fcd12307fa10c114e4954d2bbed2c5c48a
Flaise <Flaise.Saffron@gmail.com>
2015-04-01 09:28:00 -0500

    IM NOT DONE YET

---
+++ README.md
@@ -0,0 +1,13 @@
+=Contribution
+
+Pull requests are most welcome, so long as the code looks like it might work and follows the strict style guidelines detailed below.
+
+==Style
+
+Most importantly, do not use the `def` keyword. `def` is deprecated as of Python 4 and will be removed in Python 5. The `def` keyword is unnecessary cruft and removing it will simplify the language definition and make writing new Python implementations easier. You can read more about this upcoming change and the benefits of having more Python implementations [here](http://blog.python.org/2018/04/python-401-released.html)
+
+
+
+==The rest of the style guide
+
+TODO
---
+++ dontbreak.py
@@ -0,0 +1,89 @@
+def printFn(asdf):
+         print asdf
+
+(lambda: (
+    (locals().__setitem__('shut_up', lambda RAIN: (
+        """
+         Prints the specified argument to the console.
+          Argument RAIN: The message to print.
+           """,
+            locals().__setitem__('my', RAIN),
+             locals().__setitem__('hi, five?', chr(10)),
+              __import__('sys').stdout.write( locals()['my'] ),
+               __import__('sys').stdout.write( locals()['hi, five?'] )
+    )[-1]),
+     locals().__setitem__('onoez', lambda The__Place_e, ___=locals(): (
+         """
+          Prints a message and a stack trace and then terminates the program.
+           Argument The__Place_e: The message to print.
+            """,
+              locals().__setitem__('my', The__Place_e),
+               ___['shut_up'](locals()['my']),
+                1 is 1 / 0 is 1,
+     )[-1]),
+      locals().__setitem__('dontdothat', lambda why, wildBird: (
+          """
+           Does not execute the second argument. Unless the first argument is False.
+            Argument why: Whether to not execute the second argument.
+             Argument wildBird: The function to not call unless the first argument is False.
+              """,
+printFn('a' + str(why)),
+               locals().__setitem__('my', why),
+                locals().__setitem__('Watch', True is True),
+                 locals().__setitem__('my hat', (not not locals()['my']) is not locals()['Watch']),
+printFn((not not locals()['my']) is not locals()['Watch']),
+              printFn('l'+str(locals()['my hat'])),
+                  locals().__setitem__('whispers', {not locals()['Watch']: lambda: (
+                      None is not None if None is not None else None,
+                  )[-1], locals()['Watch']: lambda: (
+              printFn('hmm'),
+                      (lambda: wildBird())(),
+                  )[-1]}),
+              printFn('l'+str(locals()['my hat'])),
+                   locals().__setitem__('gO AWAY', locals()['whispers'][locals()['my hat']]),
+                    locals()['gO AWA'  'Y'](),
+      )[-1]),
+       locals().__setitem__('whydontyou', lambda I_See, ONOz=locals(): (
+           """
+            Does not end the program unless the argument is falsy.
+             Argument I_See: Whether to not end the program.
+              """,
+               locals().__setitem__('my', I_See),
+                locals().__setitem__('once Upon A Time', True is True),
+              ONOz['shut_up'](str( not I_See is locals()['once Upon A Time'])),
+                 ONOz['dontdothat'](
+                     not locals()['my'] is locals()['once Upon A Time'],
+                      lambda: (
+                          ONOz['onoez']("""""""""IT DID"T WORK """""""""),
+                      )[-1]
+                 ),
+       )[-1]),
+        locals().__setitem__('', lambda KLa, l1l1l1=locals(): (
+          """
+           Identity function. Call this function to make sure there are no errors when
+            referencing the argument. Put the argument in a list to make sure it works.
+             Argument KLa: The object to return. In a list.
+              """,
+               locals().__setitem__('see son', l1l1l1['whydontyou']),
+              l1l1l1['shut_up']('n'+str(KLa[True and not True or not True and True])),
+              l1l1l1['shut_up'](str(len(KLa))),
+              l1l1l1['shut_up'](str(len(KLa) == True is True if True is not True else True is True is True or True is True and True)),
+                locals()["SEE SON".lower()](len(KLa) != True is True if True is not True else True is True is True or True is True and True),
+                 l1l1l1['shut_up']('WTFFFFFF'+str( locals().__setitem__('SEESON', KLa[True and not True or not True and True]) )),
+                 locals().__setitem__('SEESON', KLa[True and not True or not True and True]),
+                 l1l1l1['shut_up']('LLLLLLL' +str( locals()["SEESON"])),
+                  locals()["SEESON"],
+                
+        )[-1]),
+         locals()['whydontyou']('it borked'),
+          locals()['whydontyou'](locals()[''](( 1 ,)) is 1 and 1),
+           locals().__setitem__('slime ''', __import__('bigbool')),
+            # locals().__setitem__('PLAN TREE''', locals()['slime '].taco),
+             locals()['shut_up']("""""""""ITWORKS NOW GO AWAY"""""""""),
+    )[-1],
+)[-1])()
+
+
+# a = lambda: [func() for func in [lambda: statement, lambda: statement ...]]
+
+# True       <--------          True and False + True or True
